5ea9f815d788e4837e91713c28ee5d83e65036fe
Adam SIlver <adamvs@mac.com>
2025-07-26 17:26:17 -0700

    Enclosed in module AdamsExtensions

--- cube_to_drawer.rb
+++ cube_to_drawer.rb
@@ -7,18 +7,20 @@
 require 'sketchup.rb'
 require 'extensions.rb'
 
-module HelloCube
+module AdamExtensions
+    module HelloCube
 
-    unless file_loaded?(__FILE__)
-        ex = SketchupExtension.new('Cube To Drawer', 'cube_to_drawer/ctd_main')
-        ex.description = 'SketchUp create drawer pieces from a selected cube.'
-        ex.version = '1.0.0'
-        ex.copyright = 'Adam Silver © 2025'
-        ex.creator = 'Adam Silver'
-        Sketchup.register_extension(ex, true)
-        file_loaded(__FILE__)
-    end
+        unless file_loaded?(__FILE__)
+            ex = SketchupExtension.new('Cube To Drawer', 'cube_to_drawer/ctd_main')
+            ex.description = 'SketchUp create drawer pieces from a selected cube.'
+            ex.version = '1.0.0'
+            ex.copyright = 'Adam Silver © 2025'
+            ex.creator = 'Adam Silver'
+            Sketchup.register_extension(ex, true)
+            file_loaded(__FILE__)
+        end
 
-end # module HelloCube
+    end # module HelloCube
+end # module AdamExtentions
 
 
