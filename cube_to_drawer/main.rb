b501e95cb78e03408bd76d550e91ab75f8a00fa8
Adam SIlver <adamvs@mac.com>
2025-08-12 09:36:09 -0700

    Tweeks Fixed front panel solid object issue

--- cube_to_drawer/main.rb
+++ cube_to_drawer/main.rb
@@ -22,14 +22,13 @@ module AdamExtensions
         # @param [String] context to convert thickness numeric
         def self.create_bottom_panel(face_map, thickness, units_type="metric")
             # gate this function in case face_map is empty
-            return unless face_map.key?("bottom")
+            return unless face_map!=nil && face_map.key?("bottom")
             half_thickness = thickness/2
             in_half_thickness = Utils::in_unit(half_thickness, units_type)
             bottom_rect = face_map.to_rect_copy("bottom", 0, 0, thickness, units_type)
             bottom_rect.expand(-half_thickness, -half_thickness, 0, units_type)
             mid_rect = face_map.to_rect_copy("bottom", 0, 0, half_thickness, units_type)
             mid_rect.expand(-thickness, -thickness, 0, units_type)
-            bottom_rect._prnt("bottom_rect")
             # create the group...
             model = Sketchup.active_model
             model.start_operation("Create Drawer Bottom Group", true)
@@ -41,13 +40,13 @@ module AdamExtensions
             mid_face.reverse! if mid_face.normal.z > 0
             mid_face.pushpull(in_half_thickness)
             model.commit_operation
-        end
+        end #self.create_bottom_panel
 
-        # @param [Hash] facemap faces from selected cube
+        # @param [Hash] face_map faces from selected cube
         # @param [Numeric] thickness of sides of drawer in mm
         # @param [String] context to convert thickness numeric
-        def self.create_side_panels(face_map, thickness, units_type="metric")
-            return unless face_map.key?("right")
+        def self.create_left_right_panels(face_map, thickness, units_type="metric")
+            return unless  face_map!=nil && face_map.key?("right") && face_map.key?("front")
 
             half_thickness = thickness/2
             in_thickness = Utils::in_unit(thickness, units_type)
@@ -107,20 +106,13 @@ module AdamExtensions
             # create a copy .. move to left side .. rotate 180 degrees
             front_rect = face_map.to_rect_copy("front")
             Utils::copy_move_rotate_group(right_side_group, -front_rect.width + in_thickness, 0, 0, "imperial", Z_AXIS, 180)
-            #front_rect = face_map.to_rect_copy("front")
-            #left_side_group = right_side_group.copy
-            #move_params = Geom::Transformation.new(Geom::Point3d.new(-front_rect.width + in_thickness, 0, 0))
-            #left_side_group.transform!(move_params)
-            #model.commit_operation
-            #model.start_operation("Side Left Rotate 180", true)
-            #rotate_params = Geom::Transformation.rotation(left_side_group.bounds.center, Z_AXIS, 180.degrees)
-            #left_side_group.transform!(rotate_params)
-         end
-        # @param [Hash] facemap faces from selected cube
+        end # def self.create_side_panels
+
+        # @param [Hash] face_map faces from selected cube
         # @param [Numeric] thickness of sides of drawer in mm
         # @param [String] context to convert thickness numeric
-        def self.create_side_front_back_panels(face_map, thickness, units_type="metric")
-            return unless face_map.key?("front")
+        def self.create_front_back_panels(face_map, thickness, units_type="metric")
+            return unless face_map!=nil && face_map.key?("front") && face_map.key?("left")
             model = Sketchup.active_model
             in_thickness = Utils::in_unit(thickness, units_type)
             half_thickness = thickness/2
@@ -128,17 +120,12 @@ module AdamExtensions
             base_rect = face_map.to_rect_copy("front")
 
             model.start_operation("Create Front Panel", true)
-            front_rect = base_rect.copy(0, thickness, 0, units_type)
+            front_rect = base_rect.copy()
             front_rect.expand(-half_thickness, 0, 0, units_type)
             front_group = model.entities.add_group
             front_face = front_group.entities.add_face(front_rect.points)
-            front_face.reverse! if front_face.normal.z < 0
-            front_face.pushpull(in_half_thickness)
-            front_rect.move(0, -half_thickness, 0, units_type)
-            front_rect.expand(-half_thickness, 0, 0, units_type)
-            front_face = front_group.entities.add_face(front_rect.points)
-            front_face.reverse! if front_face.normal.z < 0
-            front_face.pushpull(in_half_thickness)
+            front_face.reverse! if front_face.normal.z > 0
+            front_face.pushpull(in_thickness)
             model.commit_operation  # Create Front Panel
 
             model.start_operation("Slice Bottom Dado", true)
@@ -147,24 +134,46 @@ module AdamExtensions
             max_y = min_y + in_thickness
             min_z = base_rect.min_z + in_half_thickness
             max_z = base_rect.min_z + in_thickness
-            start_x = base_rect.max_x
-            # points going clockwise on the X, Z plane...
-            cut_rect = GeoUtil::Rect.new([Geom::Point3d.new(start_x, min_y, min_z),
-                                          Geom::Point3d.new(start_x, min_y, max_z),
-                                          Geom::Point3d.new(start_x, max_y, max_z),
-                                          Geom::Point3d.new(start_x, max_y, min_z)])
+            all_x = base_rect.max_x
+
+            #create the bottom groove...
+            cut_rect = GeoUtil::Rect.new([Geom::Point3d.new(all_x, min_y, min_z),
+                                          Geom::Point3d.new(all_x, min_y, max_z),
+                                          Geom::Point3d.new(all_x, max_y, max_z),
+                                          Geom::Point3d.new(all_x, max_y, min_z)])
             cut_group = model.entities.add_group
             cut_face = cut_group.entities.add_face(cut_rect.points)
-            cut_face.reverse! if cut_face.normal.z < 0
+            cut_face.reverse! if cut_face.normal.z > 0
             cut_face.pushpull(base_rect.width)
             front_group = cut_group.subtract(front_group)
+
+            # create the left and right insets...
+            min_y = base_rect.min_y - in_half_thickness
+            max_y = min_y + in_thickness
+            min_x = base_rect.max_x - in_thickness
+            max_x = min_x + in_thickness
+            all_z = base_rect.max_z + in_half_thickness
+            cut_rect = GeoUtil::Rect.new([Geom::Point3d.new(min_x, min_y, all_z),
+                                          Geom::Point3d.new(min_x, max_y, all_z),
+                                          Geom::Point3d.new(max_x, max_y, all_z),
+                                          Geom::Point3d.new(max_x, min_y, all_z)])
+            cut_group = model.entities.add_group
+            cut_face = cut_group.entities.add_face(cut_rect.points)
+            cut_face.reverse! if cut_face.normal.z > 0
+            cut_face.pushpull(base_rect.height + in_thickness)
+            front_group = cut_group.subtract(front_group)
+
+            cut_rect.move(-base_rect.width + in_thickness, 0, 0, "imperial")
+            cut_group = model.entities.add_group
+            cut_face = cut_group.entities.add_face(cut_rect.points)
+            cut_face.reverse! if cut_face.normal.z > 0
+            cut_face.pushpull(base_rect.height + in_thickness)
+            front_group = cut_group.subtract(front_group)
+
             side_rect = face_map.to_rect_copy("left")
             Utils::copy_move_rotate_group(front_group, 0, side_rect.depth - in_thickness, 0, "imperial", Z_AXIS, 180)
             model.commit_operation  # Slice Bottom Dado
-
-            #model.start_operation("Copy Move Front Panel", true)
-            #model.commit_operation  # Slice Bottom Dado
-        end
+        end # def self.create_side_front_back_panels
 
         #-------------------------------------------------------------------------------
         #  main Module code....
@@ -177,8 +186,8 @@ module AdamExtensions
                 sel.clear
             end
             self.create_bottom_panel(cube_map, 12)
-            self.create_side_panels(cube_map, 12)
-            self.create_side_front_back_panels(cube_map, 12)
+            self.create_left_right_panels(cube_map, 12)
+            self.create_front_back_panels(cube_map, 12)
         end # def ctd_main
 
         unless file_loaded(__FILE__)
