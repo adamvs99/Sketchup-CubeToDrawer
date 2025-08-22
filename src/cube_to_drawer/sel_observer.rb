07f8e319bc6a73ecb15cac1815292a205e6df88d
Adam Silver <adamvsilver@gmail.com>
2025-08-22 10:44:15 -0700

    Initial commit

---
+++ src/cube_to_drawer/sel_observer.rb
@@ -0,0 +1,42 @@
+#
+#  sel_observer.rb
+#
+#
+#  Created by Adam Silver on 08/14/25.
+#  copyright Adam Silver © 2025 all rights reserved
+
+require 'sketchup.rb'
+require_relative 'units_dialog'
+require_relative 'main'
+
+
+module AdamExtensions
+    module SelectObserver
+        class << self
+            attr_accessor :_sel_observer
+        end
+        self._sel_observer = nil
+
+        class SelObserver < Sketchup::SelectionObserver
+            def onSelectionBulkChange(selection)
+                # Get the model's selection
+                model = Sketchup.active_model
+                model.selection.each do |e|
+                    next unless e.is_a? Sketchup::Group
+                    if CubeToDrawer::drawer_groups.include?(e)
+                        UnitsDialog::show
+                        break
+                    end
+                end
+            end
+
+        end # class SelObserver
+
+        def self.install
+            # singletonized
+            return unless self._sel_observer.nil?
+            self._sel_observer = SelObserver.new
+            Sketchup.active_model.selection.add_observer(self._sel_observer)
+        end
+    end #module SelectObserver
+end #v UnitsDialog
