6f36c017caffd4cd6630ad7aaaee65338ceed390
Adam Silver <adamvsilver@gmail.com>
2025-09-07 16:33:23 -0700

    Added Singleton logic updates the units dialog dimensions

--- src/box_to_drawer/sel_observer.rb
+++ src/box_to_drawer/sel_observer.rb
@@ -6,36 +6,34 @@
 #  copyright Adam Silver © 2025 all rights reserved
 
 require 'sketchup.rb'
+require 'singleton'
 require_relative 'drawer'
 require_relative 'units_dialog'
 
 
 module AdamExtensions
     module SelectObserver
-        class << self
-            attr_accessor :_sel_observer
-        end
-        self._sel_observer = nil
 
         class SelObserver < Sketchup::SelectionObserver
+            include Singleton
+            def initialize
+                super()
+                Sketchup.active_model.selection.add_observer(self)
+            end
+
             def onSelectionBulkChange(selection)
                 # Get the model's selection
                 model = Sketchup.active_model
                 model.selection.each do |e|
-                    next unless e.is_a? Sketchup::Group
-                    next unless Drawer::Drawer.is_drawer_group? e
+                    next unless UnitsDialog::add_unique_selected_drawer_data(e)
                     UnitsDialog::show
-                    break
                 end
             end
 
         end # class SelObserver
 
         def self.install
-            # singletonized
-            return unless self._sel_observer.nil?
-            self._sel_observer = SelObserver.new
-            Sketchup.active_model.selection.add_observer(self._sel_observer)
+            SelObserver.instance
         end
     end #module SelectObserver
 end # module AdamExtensions
